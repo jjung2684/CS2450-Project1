49f6ab0d051d75f64f87afc549b285f11c94b9db
Spencer Barrett <srbarrett@cpp.edu>
2022-09-13 21:10:13 -0700

    Implemented both splash and home screens with basic functionality

--- SwingProjectV1.0/src/swingprojectv1/pkg0/SwingProjectV1.java
+++ SwingProjectV1.0/src/swingprojectv1/pkg0/SwingProjectV1.java
@@ -5,6 +5,16 @@
  */
 package swingprojectv1.pkg0;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
 /**
  *
  * @author barre
@@ -14,8 +24,30 @@ public class SwingProjectV1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        // Create Frame for game window
+        MainFrame mf = new MainFrame("Hangman");
+        mf.setSize(new Dimension(600, 400));
+        mf.addSplashPanel();
+        mf.setVisible(true);
+       
+
+        // Create a timer for displaying splash screen
+        Timer timer = new Timer(3000, new ActionListener() {
+
+            // After 3s change panels...
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                mf.sp.setVisible(false);
+                try {
+                    mf.addHomePanel();
+                } catch (IOException ex) {
+                    Logger.getLogger(SwingProjectV1.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        timer.setRepeats(false);
+        timer.start();
+
     }
-    
 }
