0feadf89ded9cf8270daf1adbc5c8c60a68945c6
lyhoangrivera <80252641+lyhoangrivera@users.noreply.github.com>
2022-09-18 23:33:58 -0700

    Add files via upload

---
+++ SwingProjectV1.0/src/swingprojectv1/pkg0/CreditsPanel.java
@@ -0,0 +1,108 @@
+/* ly hoang */
+package swingprojectv1.pkg0;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author tommy
+ */
+public class CreditsPanel extends JPanel implements ActionListener {
+    
+    private final MainFrame mainFrame;
+    private EndScreenPanel endScreenPanel;
+    
+    // Components
+    private JButton backButton;
+    private BufferedImage image;
+    private Font font;
+    private JLabel label;
+    
+    public CreditsPanel (MainFrame mainFrame) throws IOException {
+        backButton = new JButton("Back");
+        image = ImageIO.read(new File("/Users/lyhoang/NetBeansProjects/CS2450-Project1/Assets/Credit.jpg"));
+        //Image img = ImageIO.read(new File("background.jpg")
+        this.mainFrame = mainFrame;
+        this.setBackground(Color.white);
+        this.setLayout(null);
+        
+        // Back to home button
+        backButton = new JButton();
+        
+        backButton.setText("Back to Home");
+        backButton.setBounds(200, 300, 150, 40);
+        backButton.addActionListener(this);
+        
+        this.add(backButton);
+    }
+    
+    public Dimension getPreferredSize() {
+        return new Dimension(600, 400);
+    }
+    
+    public void paintComponent(Graphics g) {
+        font = new Font("Apple Casual", Font.BOLD, 22);
+        
+        // cast g as a Graphics2D object
+        Graphics2D g2 = (Graphics2D) g;
+        
+        super.paintComponent(g);
+
+        g2.setFont(font);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(Color.black);
+        
+        // draw image
+        g2.drawImage(image, 0, 0, null);
+
+        // draw strings
+        g2.drawString("Credits", 250, 50);
+        g2.drawString("CarlTheSpiny, 123456789", 150, 120);
+        g2.drawString("Jacob Jung, 010812009", 150, 150);
+        g2.drawString("Ly Hoang Rivera, 014384968", 150, 180);
+        g2.drawString("sbvrr, 123456789", 150, 210);
+        g2.drawString("Tommy James, 014478542", 150, 240);
+        
+
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == backButton){
+            try {
+                this.returnToHome();
+            } catch (IOException ex) {
+            }
+        }
+    }
+    
+    public void returnToHome() throws IOException {
+        mainFrame.remove(this);
+        mainFrame.repaint();
+        this.revalidate();
+        this.mainFrame.addHomePanel(mainFrame);
+    }
+    
+    
+}
+
+
